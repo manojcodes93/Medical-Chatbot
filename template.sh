e380b93efb3f6d6ec96c070fbc34cb2978b135d0
manojroyal <manojroyal7805@gmail.com>
2026-01-21 10:40:05 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+## Creating directories
+mkdir -p src
+mkdir -p research 
+
+## Creating files
+touch src/__init__
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.py
+touch requirements.txt
+
+echo "Directories and files created successfully!"
